33ce6278b86b1625a42c4804118c4bacda6541ab
Booker C. Bense <bbense@gmail.com>
2014-09-05 12:27:14 -0700

    kinda working

--- fgrep.exs
+++ fgrep.exs
@@ -23,7 +23,7 @@ defmodule PGrep do
 
 	#Search a file for a string
 	def fgrep(path,string) do
-		File.stream!(path) |> Parallel.pmap fn (line) ->  if( String.contains?(line,string), do: line ) end |> Enum.filter fn (line) -> line end
+		File.stream!(path) |> Parallel.pmap fn (line) ->  if( String.contains?(line,string), do: line ) end
 	end 
 
 
