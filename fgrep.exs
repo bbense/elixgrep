cc051feb8e97c4748a0b84e710beef065a81908d
Booker C. Bense <bbense@gmail.com>
2014-09-05 12:37:17 -0700

    Working

--- fgrep.exs
+++ fgrep.exs
@@ -23,7 +23,11 @@ defmodule PGrep do
 
 	#Search a file for a string
 	def fgrep(path,string) do
-		File.stream!(path) |> Parallel.pmap fn (line) ->  if( String.contains?(line,string), do: line ) end
+		File.stream!(path) 
+	|> 
+		Parallel.pmap(fn (line) ->  if( String.contains?(line,string), do: line ) end) 
+	|>
+		Enum.filter( fn(x) -> x end )
 	end 
 
 
