c5ba1d588913392199b3d4cbcb87261db04526d3
Booker Bense <bbense@uber.com>
2016-11-04 15:48:47 -0700

    Update to remove Elixir 1.3 warnings

--- lib/elixgrep.ex
+++ lib/elixgrep.ex
@@ -2,24 +2,24 @@ defmodule Elixgrep do
 
   @moduledoc """
       Usage:
-        exilgrep [string] [files and/or directories]
- 
+        elixgrep [string] [files and/or directories]
+
       Options:
         -h, [--help]                # Show this help message and quit.
         -c, [--count] filecount     # Number of files to process in parallel
- 
+
       Description:
-        
+
         Runs a version of map/reduce on the file list given on the command
-        line. Requires an elixir module that implements two functions. 
+        line. Requires an elixir module that implements two functions.
 
         gr_map(options,path) -> {path,[]}
-        gr_reduce(options) Expects to recieve two kinds of messages 
+        gr_reduce(options) Expects to recieve two kinds of messages
                     { item: { path, []}}
                     { finalize: } -> Should output results and exit.
     """
 
-  @ofiles_per_core 8 
+  @ofiles_per_core 8
   @plugin_path ["./plugins"]
   @plugin_funcs [gr_reduce: 1, gr_map: 2]
 
@@ -27,27 +27,27 @@ defmodule Elixgrep do
 
   require DirWalker
 
-  def gr_reduce(options) do 
+  def gr_reduce(options) do
         receive do
-          { :item, path, results } ->  
+          { :item, path, results } ->
             results |> Enum.map(fn(str) -> IO.write("#{path}: #{str}") end )
             gr_reduce(options)
 
-          { :finalize } -> 
+          { :finalize } ->
             send options.master_pid, { :all_done_boss }
             exit(:normal)
 
-        end 
-  end 
+        end
+  end
 
   def gr_map(options,path) do
-    %{ search: string } = options 
+    %{ search: string } = options
       File.stream!(path)
     |>
       Stream.filter(fn(line) -> String.contains?(line,string) end )
-    |> 
+    |>
       Enum.map( fn(x) -> x end )
-  end 
+  end
 
   def start_reduce(options) do
     spawn_link(fn -> options.reduce_func.(options) end)
@@ -56,17 +56,17 @@ defmodule Elixgrep do
   def main(args) do
       args |> parse_args |> build_paths |> background |> process |> cleanup
   end
-  
+
   def merge_opts(opts,bad_opts) do
     bad_opts |>  rehabilitate_args |> Keyword.merge(opts)
-  end 
+  end
 
   def parse_args(args) do
     options = %{ :count => @ofiles_per_core * processing_units ,
                  :map_func => fn(opt,path) -> gr_map(opt,path) end ,
                  :reduce_func => fn(opt) -> gr_reduce(opt) end }
 
-    cmd_opts = OptionParser.parse(args, 
+    cmd_opts = OptionParser.parse(args,
                                   switches: [help: :boolean , count: :integer, plugin: :string],
                                   aliases: [h: :help, c: :count, p: :plugin])
 
@@ -80,71 +80,73 @@ defmodule Elixgrep do
   end
 
   def rehabilitate_args(bad_args) do
-      bad_args 
-     |> 
+      bad_args
+     |>
       Enum.flat_map(fn(x) -> Tuple.to_list(x) end)
-     |> 
+     |>
       Enum.filter_map(fn(str) -> str end, fn(str) -> String.replace(str, ~r/^\-([^-]+)/, "--\\1") end )
      |>
       OptionParser.parse
-     |> 
+     |>
       Tuple.to_list
      |>
       List.first
   end
 
-  # Avoid expanding the stream. 
-  def build_paths({options,[head | tail]}) do  
+  # Avoid expanding the stream.
+  def build_paths({options,[head | tail]}) do
      next_opts = options |> Map.put(:search,head)
     { next_opts, DirWalker.stream(tail) }
-  end 
-  
+  end
+
   def build_paths(:help) do
     IO.puts @moduledoc
     System.halt(0)
   end
 
-  def background({options,filestream}) do 
-    if(Map.has_key?(options,:plugin), do: next_opt = load_plugin(options), else: next_opt = options)
-    reduce_opts = Map.put(next_opt,:master_pid,self()) 
+  def background({options, filestream}) do
+    next_opt =
+      case Map.has_key?(options, :plugin) do
+        true -> load_plugin(options)
+        _    -> options
+      end
+    reduce_opts = Map.put(next_opt, :master_pid, self())
     pid = start_reduce(reduce_opts)
-    {next_opt |> Map.put(:reduce_pid,pid), filestream }
+    { Map.put(next_opt, :reduce_pid, pid), filestream }
   end
- 
+
   def process({options,[path]}) do
     send options.reduce_pid, { :item,path,options.map_func.(options,path) }
     options
-  end 
+  end
 
-  def process({options,filestream}) do 
-     filestream
-    |> 
-      Stream.chunk(options.count,options.count,[])
-    |>
-      Enum.map(fn(filelist) -> Parallel.pmap(filelist, fn(path) -> process({options,[path]}) end ) end )
+  def process({options,filestream}) do
+    filestream
+    |> Stream.chunk(options.count,options.count,[])
+    |> Enum.map(fn(filelist) -> Parallel.pmap(filelist, fn(path) -> process({options,[path]}) end ) end )
     send options.reduce_pid, { :finalize }
-  end 
-  
-  def cleanup({ :finalize }) do 
+  end
+
+  def cleanup({ :finalize }) do
     receive do
-      { :all_done_boss } -> 
+      { :all_done_boss } ->
         System.halt(0)
-    end 
-  end 
+    end
+  end
 
   # We really need the options in this version.
   def cleanup(options) do
     send options.reduce_pid, { :finalize }
-    receive do 
-       { :all_done_boss } -> 
+    receive do
+       { :all_done_boss } ->
         System.halt(0)
-    end 
-  end 
+    end
+  end
 
   # Will fail with match error, which is sub-optimal
   def load_plugin(options) do
     {:ok, plugin } = Pluginator.load_with_signature(options.plugin,@plugin_funcs,@plugin_path)
-    new_opts = %{ 
+    new_opts = %{
                  :map_func => fn(opt,path) -> plugin.gr_map(opt,path) end ,
                  :reduce_func => fn(opt) -> plugin.gr_reduce(opt) end }
     Map.merge(options,new_opts)
--- plugins/find.exs
+++ plugins/find.exs
@@ -1,21 +1,21 @@
 defmodule EgPlugin.Find do
-  
+
   @moduledoc """
-  This is a plugin to duplicate some of the functionality of the find command. 
+  This is a plugin to duplicate some of the functionality of the find command.
   It also demonstrates how a plugin can use the wildcard command line options.
-  The syntax is as follows 
+  The syntax is as follows
 
       --plugin find --atime file_or_parsable_time_string [verb] [paths or files]
-  
-  For time based attributes the verbs are older, newer, around 
-  The default range for around is 24hrs, use `--delta seconds` to change it. 
+
+  For time based attributes the verbs are older, newer, around
+  The default range for around is 24hrs, use `--delta seconds` to change it.
   For now it just supports the time based parameters in File.Stat
       [ atime, ctime, mtime ]
 
   Without any additional arguments it assumes the verb is a string regexp
-  and matchs the basename against the regexp. 
+  and matchs the basename against the regexp.
 
-  * ToDo: Figure out how to have -h in the main program call help in the plugin. 
+  * ToDo: Figure out how to have -h in the main program call help in the plugin.
   * ToDo: Add parsable time string parsing.
   * ToDo: Add find style delta parsing
 
@@ -27,75 +27,76 @@ defmodule EgPlugin.Find do
              d       day (24 hours)
              w       week (7 days)
 
-         Any number of units may be combined in one `--delta` argument, for example, `--delta 1h30m`.  
+         Any number of units may be combined in one `--delta` argument, for example, `--delta 1h30m`.
   """
 
   @default_delta 86400
 
   @doc """
-  The gr_reduce function collects the results of applying gr_map to each file. 
-  The results parameter in the item bracket is exactly the output from the gr_map function on the 
-  given file path. 
+  The gr_reduce function collects the results of applying gr_map to each file.
+  The results parameter in the item bracket is exactly the output from the gr_map function on the
+  given file path.
   """
-  def gr_reduce(options) do 
+  def gr_reduce(options) do
         receive do
-          { :item, path, results } ->  
+          { :item, path, results } ->
             results |> Enum.map(fn(str) -> IO.puts("#{path}: #{str}") end )
             gr_reduce(options)
 
-          { :finalize } -> 
+          { :finalize } ->
             IO.puts("Signing off from find plugin")
             send options.master_pid, { :all_done_boss }
             exit(:normal)
 
-        end 
-  end 
+        end
+  end
 
   @doc """
   elixgrep runs this function in parallel on every file in it's expanded
-  pathlist. The search string is always the first string in the cmd line that is 
+  pathlist. The search string is always the first string in the cmd line that is
   not an arg.
   """
   def gr_map(options,path) do
-    %{ search: string } = options 
-    
-    case string do 
+    %{ search: string } = options
+
+    case string do
       "older"  -> compare_time(fn(a,b) -> a > b end,options,path)
       "newer"  -> compare_time(fn(a,b) -> a < b end,options,path)
       "around" -> compare_time(fn(a,b) -> around(options,a,b) end,options,path)
       _        -> match_name(options,path)
-    end 
+    end
     |> if( do: [string], else: [] )
   end
 
   def match_name(options,path) do
-    %{ search: string } = options 
+    %{ search: string } = options
     re = Regex.compile!(string)
     Regex.match?(re,Path.basename(path))
-  end 
+  end
 
   def compare_time(judge,options,path) do
-    case options do 
+    case options do
      %{ atime: arg } -> judge.(file_time(arg,:atime),file_time(path,:atime))
      %{ ctime: arg } -> judge.(file_time(arg,:ctime),file_time(path,:ctime))
      %{ mtime: arg } -> judge.(file_time(arg,:mtime),file_time(path,:mtime))
      _               -> raise "compare_time called w/o time choice in #{inspect options}"
-    end 
+    end
 
-  end 
+  end
 
-  def around(options,a,b) do 
-    case options do 
-      %{ delta: interval } -> value = String.to_integer(interval)
-      _                    -> value = @default_delta
-    end 
+  def around(options,a,b) do
+    value =
+      case options do
+        %{ delta: interval } -> String.to_integer(interval)
+        _                    -> @default_delta
+      end
     if(abs(a - b) < value , do: true , else: false )
-  end 
+  end
 
   def file_time(path,time_value) do
     File.stat!(path,[time: :posix])
     |> Map.get(time_value)
-  end 
+  end
 
 
 # iex(48)st = %File.Stat{access: :read_write, atime: {{2014, 9, 17}, {13, 50, 51}},
@@ -107,4 +108,4 @@ defmodule EgPlugin.Find do
 # iex(50)> st.atime
 # {{2014, 9, 17}, {13, 50, 51}}
 
-end 
+end
